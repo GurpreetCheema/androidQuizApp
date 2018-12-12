656e61103164720b1c20664e288a92d156d62e71
GurpreetCheema <34461294+GurpreetCheema@users.noreply.github.com>
2018-12-12 18:58:35 -0500

    First Commit, work on running the gradle error

---
+++ app/src/main/java/com/gurpreetcheema/quizapp/StartingScreenActivity.java
@@ -0,0 +1,31 @@
+package com.gurpreetcheema.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+public class StartingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+
+            }
+
+
+
+        });
+    }
+    private void startQuiz() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
+}
